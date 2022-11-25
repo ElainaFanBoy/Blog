01d4cbbeee23ebd2fe5bb0e93154f16e3dfe1e67
Ch3rry <712111161@qq.com>
2022-11-25 21:56:37 +0800

    Site updated: 2022-11-25 21:56:37

---
+++ js/Typed.js
@@ -0,0 +1,1 @@
+window.onload=function(){var typed=new Typed(".subtitle",{strings:['One day mayanming will be somebody','maybe the best of things,','and no good thing ever dies.'],startDelay:100,typeSpeed:100,loop:!0,backSpeed:50,showCursor:!0})}
