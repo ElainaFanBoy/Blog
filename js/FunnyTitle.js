5e407c7d608c0b613cf750f5e987807c7e9d0512
Ch3rry <712111161@qq.com>
2022-12-05 13:30:37 +0800

    Site updated: 2022-12-05 13:30:37

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
