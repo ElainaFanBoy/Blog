efdd6c510a946d7e6c850f5ba45e36004ce8aebb
Ch3rry <712111161@qq.com>
2022-12-05 13:40:32 +0800

    Site updated: 2022-12-05 13:40:31

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -9,9 +9,10 @@ document.addEventListener('visibilitychange', function () {
     }
     else {
         $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
-        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
+        document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~';
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
+            $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
         }, 2000);
     }
 });
