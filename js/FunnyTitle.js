f89cfd9018997d0d7ff0c8b9c4b22033f77d08dd
Ch3rry <712111161@qq.com>
2022-12-10 23:07:31 +0800

    Site updated: 2022-12-10 23:07:30

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,25 +1,15 @@
- var OriginTitle = document.title;
- var titleTime;
- var titleTime2;
- var titleTime3;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
-         document.title = "Waiting...";
-         clearTimeout(titleTime);
-         clearTimeout(titleTime2);
-         clearTimeout(titleTime3);
-     }
-     else {
-         document.title = "Welcome";
-         titleTime = setTimeout(function () {
-             document.title = "Back";
-             titleTime2 = setTimeout(function () {
-                 document.title = "Welcome Back !";
-                 titleTime3 = setTimeout(function () {
-                     document.title = OriginTitle;
-                 }, 800);
-             }, 500);
-         }, 500);
-     }
- });
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
+        document.title = '👀跑哪里去了~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "https://emojipedia-us.s3.amazonaws.com/source/skype/289/cherries_1f352.png");
+        document.title = '🐖抓到你啦~'
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
